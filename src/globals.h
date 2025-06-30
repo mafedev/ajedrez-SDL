87b484f050fba05841cf6ac5947b0740c676f267
mafedev <ladrillito966@gmail.com>
2025-06-30 22:01:06 +0200

    refactor: declare and organize global variables in globals.h
    Moved global variables into a centralized globals.h file to
    improve code maintainability and readability.

---
+++ src/globals.h
@@ -0,0 +1,8 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+extern int fila, columna, filaActual, columnaActual, enroqueConf;
+extern char letraColumna, letraColumnaActual, pieza, nuevaPieza;
+extern bool turno, piezaSeleccionada, enroqueBlancas, enroqueNegras, empate;
+
+#endif
